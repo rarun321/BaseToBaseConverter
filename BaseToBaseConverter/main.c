e42d20450785390bd08890af6ddd5c478244631b
rarun321 <43283635+rarun321@users.noreply.github.com>
2019-09-12 14:33:53 -0700

    Added Simple Inputs

--- BaseToBaseConverter/main.c
+++ BaseToBaseConverter/main.c
@@ -7,9 +7,53 @@
 //
 
 #include <stdio.h>
+#include <stdbool.h>
+
+#define MaxLength 1000
+int converter(int,int);
+bool checkIfNumberIsLowerThanBase(char[], char);
+int lengthOfArray(char[]);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    char base;
+    int convertbase;
+    char valueArray[MaxLength];
+    
+      printf("Enter base: ");
+      scanf("%s", &base);
+      printf("Enter value: ");
+      scanf("%s", valueArray);
+    
+    while (checkIfNumberIsLowerThanBase(valueArray, base) == false) {
+        printf("You cannot convert this! Enter another value: ");
+        scanf("%s", valueArray);
+    }
+    
+    printf("Enter base you want converted to: ");
+    scanf("%d", &convertbase);
+    return 0;
+}
+
+bool checkIfNumberIsLowerThanBase(char valueArray[], char base){
+    for (int i = 0; i < lengthOfArray(valueArray); i++) {
+        char value = valueArray[i];
+        if (value >= base) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int lengthOfArray(char valueArray[]){
+   int arrayLength = 0;
+   for (int i =0; i < MaxLength && valueArray[i] != '\0'; i++) {
+       arrayLength++;
+   }
+   return arrayLength;
+}
+
+int converter(int number, int base){
+   
     return 0;
 }
