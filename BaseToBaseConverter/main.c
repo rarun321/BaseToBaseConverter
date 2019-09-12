ca3eae5516f9afb17075ef0fee2fa3625a78d1ce
rarun321 <43283635+rarun321@users.noreply.github.com>
2019-09-11 20:26:16 -0700

    Initial Commit

---
+++ BaseToBaseConverter/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  BaseToBaseConverter
+//
+//  Created by Rithvik Arun on 9/11/19.
+//  Copyright © 2019 Rithvik Arun. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
