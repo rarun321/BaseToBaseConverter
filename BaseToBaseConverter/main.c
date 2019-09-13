c88f7ff7e9ddce2659ec65f919249f71c0224f91
rarun321 <43283635+rarun321@users.noreply.github.com>
2019-09-13 10:17:47 -0700

    Base Converter Complete

--- BaseToBaseConverter/main.c
+++ BaseToBaseConverter/main.c
@@ -11,20 +11,21 @@
 #include <math.h>
 
 #define MaxLength 1000
-int converter(int,int);
-bool checkIfNumberIsLowerThanBase(char[], char);
+void converter(int,int);
+bool checkIfNumberIsLowerThanBase(char[], int);
 int lengthOfArray(char[]);
-int convertToBase10(char[], char);
+int convertToBase10(char[], int);
+void printArrayBackwards(int[], int);
 
 //intial ask for base, value and what base they want converted to
 int main(int argc, const char * argv[]) {
     
-    char base;
+    int base;
     int convertbase;
     char valueArray[MaxLength];
     
       printf("Enter base: ");
-      scanf("%s", &base);
+      scanf("%d", &base);
       printf("Enter value: ");
       scanf("%s", valueArray);
     
@@ -36,14 +37,14 @@ int main(int argc, const char * argv[]) {
     printf("Enter base you want converted to: ");
     scanf("%d", &convertbase);
     
-    convertToBase10((valueArray), base);
+    converter(convertToBase10((valueArray), base), convertbase);
     return 0;
 }
 
 //check if value is allowed in that paticular base
-bool checkIfNumberIsLowerThanBase(char valueArray[], char base){
+bool checkIfNumberIsLowerThanBase(char valueArray[], int base){
     for (int i = 0; i < lengthOfArray(valueArray); i++) {
-        char value = valueArray[i];
+        int value = valueArray[i] - '0';
         if (value >= base) {
             return false;
         }
@@ -61,19 +62,42 @@ int lengthOfArray(char valueArray[]){
 }
 
 // convert to base 10
-int convertToBase10(char value[] , char base){
+int convertToBase10(char value[] , int base){
     int base10Value = 0;
     int baseCount = 0;
     for (int i = lengthOfArray(value) - 1; i >= 0; i--) {
         int val = value[i] - '0';
-        int power = pow((base - '0'), baseCount);
+        int power = pow((base), baseCount);
         base10Value = base10Value + (power * val);
         baseCount++;
     }
-    return 0;
+    return base10Value;
 }
 
-int converter(int number, int base){
-   
-    return 0;
+//Converts from base 10 to other bases
+void converter(int number, int base){
+    int remainder,divider,count;
+    count = 0;
+    int finalValues[MaxLength];
+    divider = number / base;
+    remainder = number % base;
+    finalValues[count] = remainder;
+    count++;
+    while (divider != 0) {
+        remainder = divider % base;
+        divider = divider / base;
+        finalValues[count] = remainder;
+        count++;
+    }
+    printArrayBackwards(finalValues,count);
 }
+
+//prints array backwards
+void printArrayBackwards(int final[], int count){
+    for (int i = count - 1; i >= 0 ; i--) {
+        printf("%d", final[i]);
+    }
+    printf("\n");
+}
+
+
